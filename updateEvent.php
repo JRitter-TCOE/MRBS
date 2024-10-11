3d55e469d33b78001264697523238788556b8d8b
JRitter-TCOE <jritter@tcoek12.org>
2024-10-11 09:04:50 -0700

    finding update method

---
+++ updateEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "<p>Starting Script</p>";
+
+
+
+require_once './google-api-php-client--PHP8.0/vendor/autoload.php';
+
+$client = new Google_Client();
+putenv('GOOGLE_APPLICATION_CREDENTIALS=meeting-room-calendar-438119-047a144cdbca.json');
+$client->useApplicationDefaultCredentials();
+$client->setApplicationName("meeting_room_calendar");
+$client->setScopes(Google_Service_Calendar::CALENDAR);
+$client->setAccessType('offline');
+
+$service = new Google_Service_Calendar($client);
+
+
+
+$calendarId = 'c_6f68de5662878ea0012a69bc021ae1fc0a45b79a042210f65f00c42c90a6a4e6@group.calendar.google.com';
+echo json_encode($service->events);
+
+echo "<p>End Script</p>";
+
+?>
