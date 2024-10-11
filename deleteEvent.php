06a387869f9b8df2547d506ea72ac487d7fc59d6
JRitter-TCOE <jritter@tcoek12.org>
2024-10-11 09:56:15 -0700

    Testing event removal

--- deleteEvent.php
+++ deleteEvent.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "<p>Starting Script</p>";
 
+$eventId = $_POST['eventId'];
 
 
 require_once './google-api-php-client--PHP8.0/vendor/autoload.php';
@@ -19,24 +19,10 @@ $client->setAccessType('offline');
 
 $service = new Google_Service_Calendar($client);
 
-$event = new Google_Service_Calendar_Event(array(
-    'summary' => "Test Event Updated 2",
-    'description' => "Test Event",
-    'start' => array(
-        'dateTime' => "2024-10-09T10:00:00-07:00"
-    ),
-    'end' => array(
-        'dateTime' => "2024-10-09T12:00:00-07:00"
-    ),
-));
 
 
-$eventId = "b6jlel8l6brdh3p4ecjbjtvbe0";
 $calendarId = 'c_6f68de5662878ea0012a69bc021ae1fc0a45b79a042210f65f00c42c90a6a4e6@group.calendar.google.com';
 $event = $service->events->delete($calendarId, $eventId);
 
-echo json_encode($event);
-
-echo "<p>End Script</p>";
 
 ?>
